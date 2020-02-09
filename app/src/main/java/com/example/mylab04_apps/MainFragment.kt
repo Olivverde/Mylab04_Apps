9cbf1ab727e196d76f9d7e3ff2a9e832fa6d33f5
Olivverde19270 <oliver.milian00@gmail.com>
2020-02-09 11:55:07 -0600

    Initial commit

---
+++ app/src/main/java/com/example/mylab04_apps/MainFragment.kt
@@ -0,0 +1,19 @@
+package com.example.mylab04_apps
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val View = inflater.inflate(R.layout.main_fragment, container, false)
+        return View
+    }
+}
